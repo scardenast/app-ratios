c6ce0fa11bae24187637b1355efe653210923f0c
Sebastián Cárdenas <39097359+scardenast@users.noreply.github.com>
2025-06-21 20:01:01 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
 
-app = Flask(__name__)
+# Ruta absoluta a la carpeta de templates
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "templates"))
+
+app = Flask(__name__, template_folder=template_dir)
 
 from . import routes
